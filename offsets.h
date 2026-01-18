b0361bba3c7476c93fc85e56c0f18debd3268bd5
dmlunar <md.node.0@gmail.com>
2026-01-18 12:35:45 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -20,7 +20,7 @@
  * | https://lunar.sh                      |
  * |                                       |
  * | Research Laboratories                 |
- * | Donate XMR @ lunar.sh (OpenAlias)     |
+ * | Donate XMR @ 'lunar.sh' (OpenAlias)   |
  * | Copyright (C) 2022-2024               |
  * +---------------------------------------+
  *
@@ -65,5 +65,6 @@
 #define T5_Thread_Timer           0x004C06E0
 
 #endif
+
 
 
